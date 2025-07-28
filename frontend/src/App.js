406b204b7f5c927b523035cdc2da1e2c9e6df7f8
Varshney <sneha.varshney@accenture.com>
2025-07-28 19:12:50 +0530

    Updated frontend code

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import "./index.css"
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-2xl">Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,11 @@
-// tailwind.config.js
-export default {
-    content: [
-        "./index.html",
-        "./src/**/*.{js,ts,jsx,tsx}",
-    ],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+      "./src/*/.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 }
+
